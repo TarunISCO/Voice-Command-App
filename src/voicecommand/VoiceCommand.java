d6cb38f0546d34af9c079f4f60454d3f70b981ee
TarunISCO <201552063@iiitvadodara.ac.in>
2017-12-02 16:25:12 +0530

    added new commands

--- src/voicecommand/VoiceCommand.java
+++ src/voicecommand/VoiceCommand.java
@@ -8,6 +8,8 @@ package voicecommand;
 import edu.cmu.sphinx.api.Configuration;
 import edu.cmu.sphinx.api.LiveSpeechRecognizer;
 import edu.cmu.sphinx.api.SpeechResult;
+import java.awt.Desktop;
+import java.io.File;
 import java.io.IOException;
 
 /**
@@ -28,9 +30,10 @@ public class VoiceCommand {
         configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
         
         // Set path to the dictionary.
-        configuration.setDictionaryPath("/home/tarun/NetBeansProjects/VoiceCommand/0665.dic");
+        configuration.setDictionaryPath("/home/tarun/NetBeansProjects/VoiceCommand/latest.dic");
+        
         // Set path to the language model.
-        configuration.setLanguageModelPath("/home/tarun/NetBeansProjects/VoiceCommand/0665.lm");
+        configuration.setLanguageModelPath("/home/tarun/NetBeansProjects/VoiceCommand/latest.lm");
         
         // Recognizer Object, Pass the Configuration object
         LiveSpeechRecognizer recognize = new LiveSpeechRecognizer(configuration);
@@ -41,20 +44,73 @@ public class VoiceCommand {
         // Create SpeechResult object
         SpeechResult result;
         
+        Runtime mRunTime = Runtime.getRuntime();
+        
         // Checking if recogniser has recognised the speech
         while ((result = recognize.getResult()) != null) {
-            //Get the recognize speech
+            //Get the recognized speech
             String command = result.getHypothesis();
+            String work = null;
+            Process p;
             
-            //Match recognized speech with our commands
-            if(command.equalsIgnoreCase("open chrome")) {
-                System.out.println("Chrome Opened!");
-            } else if (command.equalsIgnoreCase("close chrome")) {
-                System.out.println("Chrome Closed!");
-            } else if (command.equalsIgnoreCase("open terminal")) {
-                System.out.println("Terminal Opened!");
-            } else if (command.equalsIgnoreCase("close terminal")) {
-                System.out.println("Terminal Closed!");
+            //Some Extra Commands from my Corpus File
+            if(command.equalsIgnoreCase("open search")) {
+                work = "google-chrome http://www.google.com";
+                System.out.println(command);
+            } 
+            else if (command.equalsIgnoreCase("new tab")) {
+                work = "google-chrome \\c";
+                System.out.println(command);
+            } 
+            else if (command.equalsIgnoreCase("open mail")) {
+                work = "google-chrome mail.google.com";
+                System.out.println(command);
+            } 
+            else if (command.equalsIgnoreCase("open linked in")) {
+                work = "google-chrome linkedin.com/in/tarunisco";
+                System.out.println(command);
+            } 
+            else if (command.equalsIgnoreCase("open blog")) {
+                work = "google-chrome procurity.wordpress.com";
+                System.out.println(command);
+            } 
+            else if (command.equalsIgnoreCase("open git hub")) {
+                work = "google-chrome github.com/tarunisco";
+                System.out.println(command);
+            } 
+            else if (command.equalsIgnoreCase("browser")) {
+                work = "google-chrome";
+                System.out.println(command);
+            } 
+            else if (command.equalsIgnoreCase("terminal")) {
+                work = "gnome-terminal";
+                System.out.println(command);
+            } 
+            else if (command.equalsIgnoreCase("file manager")) {
+                work = "nautilus";
+                System.out.println(command);
+            }
+            else if (command.equalsIgnoreCase("presentation")) {
+                File presentationFile = new File("/home/tarun/speech.pdf");
+                if(presentationFile.exists()) {
+                    if(Desktop.isDesktopSupported()) {
+                        Desktop.getDesktop().open(presentationFile);
+                    }
+                }
+                System.out.println("Presentation opened");
+            }
+            else if (command.equalsIgnoreCase("open paper")) {
+                File presentationFile = new File("/home/tarun/paper.pdf");
+                if(presentationFile.exists()) {
+                    if(Desktop.isDesktopSupported()) {
+                        Desktop.getDesktop().open(presentationFile);
+                    }
+                }
+                System.out.println("Paper opened");
+            }
+            
+            if(work != null) {
+                p = Runtime.getRuntime().exec(work);
             }
         }
         
